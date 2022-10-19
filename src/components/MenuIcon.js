450de8a94f1a427d1aaa8bb3cb466652a76ed44f
Lirije <lirije11@hotmail.com>
2022-10-19 22:18:25 +0200

    responsivity added and dropdown menu

--- src/components/MenuIcon.js
+++ src/components/MenuIcon.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-const MenuIcon = ({button, link, icon, quantity}) => {
+const MenuIcon = ({button, link, icon, quantity, hideOnMobile}) => {
   return (
-    <button type={button} className="btn btn-light" >
+    <button type={button} className={`btn btn-light ${hideOnMobile ? "d-none d-md-flex" : ""}`}>
         <NavLink className="menu-icon" to={link} end>
             <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-theme">{quantity}</span>
             <i className={icon}></i>
--- src/section/MainMenuSection.js
+++ src/section/MainMenuSection.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import MenuIcon from '../components/MenuIcon'
 
 const MainMenuSection = () => {
+  const [showMenu, setShowMenu] = useState(false)
+  
+  const toggleMenu = () => {
+    setShowMenu(!showMenu)
+  }
+
   return (
     <div className="menu-bar">
         <div className="container">
             <NavLink className="logo" to="/" end>Fixxo.</NavLink>
-            <div className="menu-links">
+            <div className={`menu-links ${showMenu ? "d-flex" : ""}`}>
                 <NavLink className="menu-link" to="/" end>Home</NavLink>
                 <NavLink className="menu-link" to="/categories" end>Categories</NavLink>
                 <NavLink className="menu-link" to="/products" end>Products</NavLink>
@@ -15,9 +21,12 @@ const MainMenuSection = () => {
             </div>
             <span className="circle-icon">
                 <MenuIcon link="/search" icon="fa-regular fa-magnifying-glass" />
-                <MenuIcon link="/compare" icon="fa-regular fa-code-compare" />
-                <MenuIcon quantity="3" link="/wishlist" icon="fa-regular fa-heart" />
+                <MenuIcon hideOnMobile={true} link="/compare" icon="fa-regular fa-code-compare" />
+                <MenuIcon hideOnMobile={true} quantity="3" link="/wishlist" icon="fa-regular fa-heart" />
                 <MenuIcon quantity="4" link="/shoppingcart" icon="fa-regular fa-bag-shopping" />
+            
+                <button onClick={toggleMenu} className="d-lg-none btn btn-light btn-dropdown"><i className="fa-regular fa-bars"></i></button>
+
             </span>
         </div>
     </div>
