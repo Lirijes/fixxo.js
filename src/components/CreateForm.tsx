e8ddbbbe3be8407870ed9d2d27d729ff4999b5f5
Lirije <lirije11@hotmail.com>
2022-11-30 10:00:39 +0100

    last commit, everything  in TS file now

--- src/components/CreateForm.tsx
+++ src/components/CreateForm.tsx
@@ -10,6 +10,7 @@ const CreateForm = () => {
         <input value={UserNew.firstName} onChange={(e) => setUserNew({...UserNew, firstName: e.target.value })} type="text" className="form-control py-2 mb-3" placeholder="Enter your first name..." />
         <input value={UserNew.lastName} onChange={(e) => setUserNew({...UserNew, lastName: e.target.value })} type="text" className="form-control py-2 mb-3" placeholder="Enter your last name..." />
         <input value={UserNew.email} onChange={(e) => setUserNew({...UserNew, email: e.target.value })} type="text" className="form-control py-2 mb-3" placeholder="Enter your e-mail address..." />
+        <input value={UserNew.password} onChange={(e) => setUserNew({...UserNew, password: e.target.value })} type="password" className="form-control py-2 mb-3" placeholder="Enter your password..." />
         <button type="submit" className="btn py-2 mt-3">CREATE USER</button>
       </form>
     )
